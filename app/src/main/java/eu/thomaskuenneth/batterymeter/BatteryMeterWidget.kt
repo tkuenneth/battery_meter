d42d4c1fa63438d18e6e7f0ba4f4b78a12b7c78b
Thomas Künneth <kuennetht@gmail.com>
2023-03-19 17:49:09 +0100

    First version of battery

--- app/src/main/java/eu/thomaskuenneth/batterymeter/BatteryMeterWidget.kt
+++ app/src/main/java/eu/thomaskuenneth/batterymeter/BatteryMeterWidget.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.datastore.preferences.core.floatPreferencesKey
 import androidx.datastore.preferences.core.longPreferencesKey
 import androidx.glance.GlanceModifier
@@ -21,8 +22,14 @@ import androidx.glance.currentState
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Box
 import androidx.glance.layout.Column
+import androidx.glance.layout.Row
+import androidx.glance.layout.RowScope
+import androidx.glance.layout.fillMaxHeight
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.fillMaxWidth
+import androidx.glance.layout.height
+import androidx.glance.layout.padding
+import androidx.glance.layout.width
 import androidx.glance.state.PreferencesGlanceStateDefinition
 import androidx.glance.text.Text
 import eu.thomaskuenneth.batterymeter.BatteryMeterWidgetReceiver.Companion.batteryPercent
@@ -43,7 +50,7 @@ class BatteryMeterWidget : GlanceAppWidget() {
         val context = LocalContext.current
         Column(
             modifier = GlanceModifier.fillMaxSize()
-                .background(color = Color.White),
+                .background(color = Color.Transparent),
             horizontalAlignment = Alignment.Horizontal.CenterHorizontally
         ) {
             Box(
@@ -51,9 +58,36 @@ class BatteryMeterWidget : GlanceAppWidget() {
                 contentAlignment = Alignment.Center
             ) {
                 if (percent != null) {
-                    Text(
-                        text = "${percent.toInt()}%"
-                    )
+                    Row(
+                        modifier = GlanceModifier.fillMaxSize(),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Box(
+                            modifier = GlanceModifier.defaultWeight()
+                                .fillMaxHeight()
+                                .padding(vertical = 8.dp),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Row(
+                                modifier = GlanceModifier.fillMaxSize()
+                                    .background(color = Color.Gray)
+                                    .padding(all = 6.dp)
+                            ) {
+                                with(percent.toInt() + 9) {
+                                    for (i in 1..100 step 10) {
+                                        Segment(i <= percent)
+                                    }
+                                }
+                            }
+                            Text(
+                                text = "${percent.toInt()}%"
+                            )
+                        }
+                        Box(
+                            modifier = GlanceModifier.width(8.dp).height(16.dp)
+                                .background(color = Color.Gray)
+                        ) {}
+                    }
                 } else {
                     CircularProgressIndicator()
                     SideEffect {
@@ -63,6 +97,7 @@ class BatteryMeterWidget : GlanceAppWidget() {
             }
             if (updated != null) {
                 Text(
+                    modifier = GlanceModifier.background(color = Color.White),
                     text = SimpleDateFormat.getDateTimeInstance().format(Date(updated))
                 )
             }
@@ -74,6 +109,16 @@ class BatteryMeterWidget : GlanceAppWidget() {
         ) {
         }
         LocalContext.current.appendTextToFile("Content()")
+    }
+}
+
+@Composable
+private fun RowScope.Segment(isFilled: Boolean) {
+    Box(
+        modifier = GlanceModifier.defaultWeight()
+            .fillMaxHeight()
+            .background(color = if (isFilled) Color.Green else Color.Transparent)
+    ) {
     }
 }
 
